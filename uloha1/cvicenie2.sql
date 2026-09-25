713683a92fc0b22133530276822dddeb549250e6
RichardBogdan <richardinobogdan@gmail.com>
2026-09-25 17:36:57 +0200

    Úloha 3 - Prepojenie objednávok, zákazníkov a produktov

--- uloha1/cvicenie2.sql
+++ uloha1/cvicenie2.sql
@@ -6,3 +6,9 @@ SELECT * FROM orders;
 SELECT orders.order_id, customers.customer_name, orders.sales FROM orders
 INNER JOIN customers ON orders.customer_id = customers.customer_id
 WHERE orders.sales > 500;  
+
+/*Úloha 3*/
+SELECT orders.order_id, customers.customer_name, products.category, orders.sales FROM orders
+INNER JOIN customers ON orders.customer_id = customers.customer_id
+INNER JOIN products ON orders.product_id = products.product_id;
+
