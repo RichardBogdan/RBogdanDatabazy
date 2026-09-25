7b22a085df8adcf716e89481f0c16fcbda771769
RichardBogdan <richardinobogdan@gmail.com>
2026-09-25 17:25:09 +0200

    Úloha 2 - Filtrovanie objednávok podľa hodnoty

---
+++ uloha1/cvicenie2.sql
@@ -0,0 +1,8 @@
+SELECT * FROM customers;
+SELECT * FROM products;
+SELECT * FROM orders;
+
+/*Úloha 2*/
+SELECT orders.order_id, customers.customer_name, orders.sales FROM orders
+INNER JOIN customers ON orders.customer_id = customers.customer_id
+WHERE orders.sales > 500;  
